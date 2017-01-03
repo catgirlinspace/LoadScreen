e412d25cd0473f35074dae5ebf6f47f067a6cbbf
Dog2puppy <colecooper2005@icloud.com>
2017-01-03 16:20:25 -0500

    Update Main.php

--- src/Dog2puppy/LoadScreen/Main.php
+++ src/Dog2puppy/LoadScreen/Main.php
@@ -2,7 +2,7 @@
 
 Namespace Dog2puppy\LoadScreen;
 
-use pocketmine\plugin\PluginBase
+use pocketmine\plugin\PluginBase;
 use pocketmine\network\protocol\ChangeDimensionPacket;
 use pocketmine\event\Listener;
 use pocketmine\command\Command;
@@ -14,5 +14,23 @@ class Main extends PluginBase implements Listener;
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
   }
   public function onCommand(Command $command, CommandSender $sender, $label, Array $args){
-    
+    $x= $sender->getX();
+    $y= $sender->getY();
+    $z= $sender->getZ();
+    $pk = new ChangeDimensionPacket();
+    $pk->dimension = 1; //1 is the nether; 0 is the overworld
+    $pk->x = (int) $x; //define X
+    $pk->y = (int) $y; //define Y
+    $pk->z = (int) $z; //define Z
+    $player->dataPacket($pk); //$player must be an instance of a pocketmine/Player
+    $pk = new ChangeDimensionPacket();
+    $pk->dimension = 0; //1 is the nether; 0 is the overworld
+    $pk->x = (int) $x; //define X
+    $pk->y = (int) $y; //define Y
+    $pk->z = (int) $z; //define Z
+    $player->dataPacket($pk); //$player must be an instance of a pocketmine/Player
+    if($args[0]=="message"){
+      $player->sendMessage("[LoadScreen] Showed the loading screen");
+    }
+  } 
 }
